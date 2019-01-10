b0147459691827a393abdbc6fe3c790e0cf1778c
Karan Jaiswal <33316049+karanjaiswal0000@users.noreply.github.com>
2019-01-11 00:44:06 +0530

    Project Added!
    Project basic functions working still some functions need work.

---
+++ TicTacToe/Main.Designer.vb
@@ -0,0 +1,387 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Home
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.NewGameMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SinglePlayerMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MultiPlayerMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ResetMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.TitleLabel = New System.Windows.Forms.Label()
+        Me.ScoresGroupBox = New System.Windows.Forms.GroupBox()
+        Me.PlayerOScoreLabel = New System.Windows.Forms.Label()
+        Me.PlayerOLabel = New System.Windows.Forms.Label()
+        Me.PlayerXScoreLabel = New System.Windows.Forms.Label()
+        Me.PlayerXLabel = New System.Windows.Forms.Label()
+        Me.MinimizeButton = New System.Windows.Forms.Button()
+        Me.CloseButton = New System.Windows.Forms.Button()
+        Me.StatusLabel = New System.Windows.Forms.Label()
+        Me.MenuStrip.SuspendLayout()
+        Me.ScoresGroupBox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip
+        '
+        Me.MenuStrip.AutoSize = False
+        Me.MenuStrip.BackColor = System.Drawing.Color.DarkBlue
+        Me.MenuStrip.Font = New System.Drawing.Font("Segoe UI Semibold", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewGameMenuItem, Me.ResetMenuItem, Me.AboutMenuItem})
+        Me.MenuStrip.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow
+        Me.MenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip.Name = "MenuStrip"
+        Me.MenuStrip.Size = New System.Drawing.Size(650, 50)
+        Me.MenuStrip.TabIndex = 0
+        '
+        'NewGameMenuItem
+        '
+        Me.NewGameMenuItem.AutoSize = False
+        Me.NewGameMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.NewGameMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SinglePlayerMenuItem, Me.MultiPlayerMenuItem})
+        Me.NewGameMenuItem.ForeColor = System.Drawing.Color.White
+        Me.NewGameMenuItem.Name = "NewGameMenuItem"
+        Me.NewGameMenuItem.Size = New System.Drawing.Size(122, 36)
+        Me.NewGameMenuItem.Text = "New Game"
+        '
+        'SinglePlayerMenuItem
+        '
+        Me.SinglePlayerMenuItem.BackColor = System.Drawing.Color.White
+        Me.SinglePlayerMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.SinglePlayerMenuItem.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SinglePlayerMenuItem.ForeColor = System.Drawing.Color.DarkBlue
+        Me.SinglePlayerMenuItem.Name = "SinglePlayerMenuItem"
+        Me.SinglePlayerMenuItem.Size = New System.Drawing.Size(173, 26)
+        Me.SinglePlayerMenuItem.Text = "Single Player"
+        '
+        'MultiPlayerMenuItem
+        '
+        Me.MultiPlayerMenuItem.BackColor = System.Drawing.Color.White
+        Me.MultiPlayerMenuItem.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MultiPlayerMenuItem.ForeColor = System.Drawing.Color.DarkBlue
+        Me.MultiPlayerMenuItem.Name = "MultiPlayerMenuItem"
+        Me.MultiPlayerMenuItem.Size = New System.Drawing.Size(173, 26)
+        Me.MultiPlayerMenuItem.Text = "Multi Player"
+        '
+        'ResetMenuItem
+        '
+        Me.ResetMenuItem.AutoSize = False
+        Me.ResetMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ResetMenuItem.ForeColor = System.Drawing.Color.White
+        Me.ResetMenuItem.Name = "ResetMenuItem"
+        Me.ResetMenuItem.Size = New System.Drawing.Size(80, 36)
+        Me.ResetMenuItem.Text = "Reset"
+        '
+        'AboutMenuItem
+        '
+        Me.AboutMenuItem.AutoSize = False
+        Me.AboutMenuItem.ForeColor = System.Drawing.Color.White
+        Me.AboutMenuItem.Name = "AboutMenuItem"
+        Me.AboutMenuItem.Size = New System.Drawing.Size(100, 36)
+        Me.AboutMenuItem.Text = "About"
+        '
+        'Button1
+        '
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button1.FlatAppearance.BorderSize = 2
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Black
+        Me.Button1.Location = New System.Drawing.Point(50, 100)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 100)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button2.FlatAppearance.BorderSize = 2
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Black
+        Me.Button2.Location = New System.Drawing.Point(175, 100)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 100)
+        Me.Button2.TabIndex = 0
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button3.FlatAppearance.BorderSize = 2
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.Black
+        Me.Button3.Location = New System.Drawing.Point(300, 100)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 100)
+        Me.Button3.TabIndex = 0
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button4.FlatAppearance.BorderSize = 2
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.ForeColor = System.Drawing.Color.Black
+        Me.Button4.Location = New System.Drawing.Point(50, 225)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 100)
+        Me.Button4.TabIndex = 0
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button5.FlatAppearance.BorderSize = 2
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.ForeColor = System.Drawing.Color.Black
+        Me.Button5.Location = New System.Drawing.Point(175, 225)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(100, 100)
+        Me.Button5.TabIndex = 0
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button6.FlatAppearance.BorderSize = 2
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.ForeColor = System.Drawing.Color.Black
+        Me.Button6.Location = New System.Drawing.Point(300, 225)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(100, 100)
+        Me.Button6.TabIndex = 0
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button7.FlatAppearance.BorderSize = 2
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.ForeColor = System.Drawing.Color.Black
+        Me.Button7.Location = New System.Drawing.Point(50, 350)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(100, 100)
+        Me.Button7.TabIndex = 0
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button8.FlatAppearance.BorderSize = 2
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button8.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.ForeColor = System.Drawing.Color.Black
+        Me.Button8.Location = New System.Drawing.Point(175, 350)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(100, 100)
+        Me.Button8.TabIndex = 0
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button9.FlatAppearance.BorderSize = 2
+        Me.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button9.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.ForeColor = System.Drawing.Color.Black
+        Me.Button9.Location = New System.Drawing.Point(300, 350)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(100, 100)
+        Me.Button9.TabIndex = 0
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'TitleLabel
+        '
+        Me.TitleLabel.Font = New System.Drawing.Font("Segoe Print", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TitleLabel.ForeColor = System.Drawing.Color.DarkBlue
+        Me.TitleLabel.Location = New System.Drawing.Point(410, 75)
+        Me.TitleLabel.Name = "TitleLabel"
+        Me.TitleLabel.Size = New System.Drawing.Size(230, 50)
+        Me.TitleLabel.TabIndex = 0
+        Me.TitleLabel.Text = "Tic-Tac-Toe"
+        Me.TitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ScoresGroupBox
+        '
+        Me.ScoresGroupBox.BackColor = System.Drawing.Color.Transparent
+        Me.ScoresGroupBox.Controls.Add(Me.PlayerOScoreLabel)
+        Me.ScoresGroupBox.Controls.Add(Me.PlayerOLabel)
+        Me.ScoresGroupBox.Controls.Add(Me.PlayerXScoreLabel)
+        Me.ScoresGroupBox.Controls.Add(Me.PlayerXLabel)
+        Me.ScoresGroupBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ScoresGroupBox.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ScoresGroupBox.ForeColor = System.Drawing.Color.DarkBlue
+        Me.ScoresGroupBox.Location = New System.Drawing.Point(435, 300)
+        Me.ScoresGroupBox.Name = "ScoresGroupBox"
+        Me.ScoresGroupBox.Size = New System.Drawing.Size(180, 150)
+        Me.ScoresGroupBox.TabIndex = 0
+        Me.ScoresGroupBox.TabStop = False
+        Me.ScoresGroupBox.Text = "Scores"
+        '
+        'PlayerOScoreLabel
+        '
+        Me.PlayerOScoreLabel.Location = New System.Drawing.Point(100, 100)
+        Me.PlayerOScoreLabel.Name = "PlayerOScoreLabel"
+        Me.PlayerOScoreLabel.Size = New System.Drawing.Size(50, 25)
+        Me.PlayerOScoreLabel.TabIndex = 0
+        Me.PlayerOScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PlayerOLabel
+        '
+        Me.PlayerOLabel.Location = New System.Drawing.Point(25, 100)
+        Me.PlayerOLabel.Name = "PlayerOLabel"
+        Me.PlayerOLabel.Size = New System.Drawing.Size(50, 25)
+        Me.PlayerOLabel.TabIndex = 0
+        Me.PlayerOLabel.Text = "O:"
+        Me.PlayerOLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PlayerXScoreLabel
+        '
+        Me.PlayerXScoreLabel.Location = New System.Drawing.Point(100, 50)
+        Me.PlayerXScoreLabel.Name = "PlayerXScoreLabel"
+        Me.PlayerXScoreLabel.Size = New System.Drawing.Size(50, 25)
+        Me.PlayerXScoreLabel.TabIndex = 0
+        Me.PlayerXScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PlayerXLabel
+        '
+        Me.PlayerXLabel.Location = New System.Drawing.Point(25, 50)
+        Me.PlayerXLabel.Name = "PlayerXLabel"
+        Me.PlayerXLabel.Size = New System.Drawing.Size(50, 25)
+        Me.PlayerXLabel.TabIndex = 0
+        Me.PlayerXLabel.Text = "X:"
+        Me.PlayerXLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'MinimizeButton
+        '
+        Me.MinimizeButton.BackColor = System.Drawing.Color.Orange
+        Me.MinimizeButton.BackgroundImage = Global.TicTacToe.My.Resources.Resources.Minimize
+        Me.MinimizeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.MinimizeButton.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.MinimizeButton.FlatAppearance.BorderSize = 2
+        Me.MinimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.MinimizeButton.Location = New System.Drawing.Point(566, 10)
+        Me.MinimizeButton.Name = "MinimizeButton"
+        Me.MinimizeButton.Size = New System.Drawing.Size(32, 32)
+        Me.MinimizeButton.TabIndex = 10
+        Me.MinimizeButton.UseVisualStyleBackColor = False
+        '
+        'CloseButton
+        '
+        Me.CloseButton.BackColor = System.Drawing.Color.Red
+        Me.CloseButton.BackgroundImage = Global.TicTacToe.My.Resources.Resources.Close
+        Me.CloseButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.CloseButton.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.CloseButton.FlatAppearance.BorderSize = 2
+        Me.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.CloseButton.Location = New System.Drawing.Point(608, 10)
+        Me.CloseButton.Name = "CloseButton"
+        Me.CloseButton.Size = New System.Drawing.Size(32, 32)
+        Me.CloseButton.TabIndex = 11
+        Me.CloseButton.UseVisualStyleBackColor = False
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.StatusLabel.ForeColor = System.Drawing.Color.Black
+        Me.StatusLabel.Location = New System.Drawing.Point(435, 175)
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Size = New System.Drawing.Size(180, 50)
+        Me.StatusLabel.TabIndex = 0
+        Me.StatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Home
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(650, 500)
+        Me.Controls.Add(Me.StatusLabel)
+        Me.Controls.Add(Me.CloseButton)
+        Me.Controls.Add(Me.MinimizeButton)
+        Me.Controls.Add(Me.ScoresGroupBox)
+        Me.Controls.Add(Me.TitleLabel)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip)
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.DarkBlue
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MainMenuStrip = Me.MenuStrip
+        Me.Name = "Home"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tic-Tac-Toe"
+        Me.MenuStrip.ResumeLayout(False)
+        Me.MenuStrip.PerformLayout()
+        Me.ScoresGroupBox.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents MenuStrip As MenuStrip
+    Friend WithEvents NewGameMenuItem As ToolStripMenuItem
+    Friend WithEvents ResetMenuItem As ToolStripMenuItem
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents TitleLabel As Label
+    Friend WithEvents ScoresGroupBox As GroupBox
+    Friend WithEvents PlayerOScoreLabel As Label
+    Friend WithEvents PlayerOLabel As Label
+    Friend WithEvents PlayerXScoreLabel As Label
+    Friend WithEvents PlayerXLabel As Label
+    Friend WithEvents SinglePlayerMenuItem As ToolStripMenuItem
+    Friend WithEvents MultiPlayerMenuItem As ToolStripMenuItem
+    Friend WithEvents MinimizeButton As Button
+    Friend WithEvents CloseButton As Button
+    Friend WithEvents AboutMenuItem As ToolStripMenuItem
+    Friend WithEvents StatusLabel As Label
+End Class
